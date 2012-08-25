97d52a3b32377932184ae933e668a905d264762b
ivan.lam <ivanlin96@gmail.com>
2012-08-26 00:38:27 +0800

    Add sample methods.

--- Sample/ViewController.m
+++ Sample/ViewController.m
@@ -1,12 +1,21 @@
-//
-//  ViewController.m
-//  Sample
-//
-//  Created by Ivan Lam on 12-8-19.
-//  Copyright (c) 2012年 Ivan Lam. All rights reserved.
-//
+/*
+ Copyright Ivan Lam
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "ViewController.h"
+#import "EasyValidate.h"
 
 @interface ViewController ()
 
@@ -18,6 +27,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.email.checkRule = @"Email";
+    self.email.title = @"Email";
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +37,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)checkAll:(id)sender {
+    NSArray *elements = {self.email, self.mobile, self.digit};
+    EasyValidate *easyValidate = [[EasyValidate alloc] init];
+    [easyValidate checkOneElement:self.email withAlert:YES];
+}
 @end
